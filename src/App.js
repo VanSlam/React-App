89cffeba8dccc6c5b17845b49139edf718291825
Zachary Van Slambrook <zvanslambrook@yahoo.com>
2019-06-21 14:22:15 -0400

    017-final-touches

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 //Zach Van Slambrook
 import React from 'react'
 import { render } from 'react-dom'
-import SearchParams from './SearchParam.js'
-
-/*const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h3', {}, breed)
-  ])
-}*/
+import SearchParams from './SearchParam'
 import Pet from './Pet'
-
-/*const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt me!'),
-    React.createElement(Pet, { name: 'Tucker', animal: 'dog', breed: 'Mut' }),
-    React.createElement(Pet, { name: 'scout', animal: 'dog', breed: 'ausy' }),
-    React.createElement(Pet, {
-      name: 'freddy',
-      animal: 'frog',
-      breed: 'bullfrog'
-    }),
-    React.createElement(Pet, { name: 'tootles', animal: 'cat', breed: 'tabby' })
-  ])
-}*/
+import { Router } from '@reach/router'
+import Details from './Details'
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   )
 }
---
+++ src/Details.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import pet from '@frontendmasters/pet'
+
+const Details = ({ id }) => {
+  // ya know...
+  // the most recent pet info for pet
+  // can be found from the API
+
+  const [selectedPet, setSelectedPet] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const [isSuccessful, setIsSuccessful] = useState({
+    success: false,
+    err: null
+  })
+
+  async function getPet(petId) {
+    // const { animal } = await pet.animal(petId)
+    pet
+      .animal(petId)
+      .then(animal => {
+        setSelectedPet(animal)
+        setIsLoading(false)
+        setIsSuccessful({ success: true })
+      })
+      .catch(err => {
+        setSelectedPet({})
+        setIsLoading(false)
+        setIsSuccessful({ success: false, err })
+      })
+  }
+
+  useEffect(() => {
+    getPet(id)
+  }, [])
+  let DisplayTheStuff = null
+  if (isLoading === false && isSuccessful.success === true) {
+    DisplayTheStuff = (
+      <div>
+        <h1>Name: {selectedPet.animal.name}</h1>
+        <h2>Breed: {selectedPet.animal.breeds.primary}</h2>
+        <h2>Gender: {selectedPet.animal.gender}</h2>
+        <p>{selectedPet.animal.description}</p>
+      </div>
+    )
+  } else if (isLoading === true) {
+    DisplayTheStuff = (
+      <div>
+        <h1>Loading... hang on...</h1>
+      </div>
+    )
+  } else {
+    DisplayTheStuff = <div>Whoops, couldnt get the pet.</div>
+  }
+
+  return DisplayTheStuff
+}
+
+export default Details
